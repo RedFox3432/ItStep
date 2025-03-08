ae79bf85d183b433e7f10e7485c6efd71274b00d
RedFox3432 <eroryakimenko13@gmail.com>
2025-03-08 14:27:52 +0200

    дз1

--- file 1.py
+++ file 1.py
@@ -1,2 +1,11 @@
-print ("hello world")
-#fff
+print ("пітвердження особистості")
+print ('='*25,'\033[0m\n')
+age=int(input('ващ вік: '))
+if age >=0 and age <14:
+    print('свідоцтво про народження')
+elif 14<=age<=35 :
+    print('id-паспорт')
+elif 35<=age<=110 :
+    print('паспорт страого зразку')
+else:
+    print('помилка')
